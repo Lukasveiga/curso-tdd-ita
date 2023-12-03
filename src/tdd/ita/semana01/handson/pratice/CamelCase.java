3e48244277675e7c86e8ad7692241e107846b8b6
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-03 19:39:28 -0300

    Refactor convertCamelCase to pass in a second test

--- src/tdd/ita/semana01/handson/pratice/CamelCase.java
+++ src/tdd/ita/semana01/handson/pratice/CamelCase.java
@@ -1,10 +1,20 @@
 package tdd.ita.semana01.handson.pratice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CamelCase {
     public static List<String> convertCamelCase(String input) {
-        String result = input.substring(0,1).toLowerCase() + input.substring(1);
-        return List.of(result);
+        var resultList = new ArrayList<String>();
+
+        var splitedInput = input.trim().split("(?=[A-Z])|(?<=[a-z])(?=[A-Z])");
+
+        for(String word : splitedInput) {
+            word = word.trim();
+            var result = word.substring(0,1).toLowerCase() + word.substring(1);
+            resultList.add(result);
+        }
+
+        return resultList;
     }
 }
