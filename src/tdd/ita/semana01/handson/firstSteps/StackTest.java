af0e293d8917e9576f4f806cc30f0a9c9d2d65ed
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-03 14:40:46 -0300

    Refactor test inserto beforeEach stack initialization

--- src/tdd/ita/semana01/handson/firstSteps/StackTest.java
+++ src/tdd/ita/semana01/handson/firstSteps/StackTest.java
@@ -1,20 +1,24 @@
 package tdd.ita.semana01.handson.firstSteps;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StackTest {
 
+    private MyStack<String> stack;
+    @BeforeEach
+    public void initStack() {
+        stack = new MyStack<>();
+    }
+
     @Test
     public void emptyStack() {
-        var stack = new MyStack<String>();
         assertTrue(stack.isEmpty());
         assertEquals(0, stack.size());
     }
 
     @Test
     public void stackAnElement() {
-        var stack = new MyStack<String>();
         stack.add("first");
         assertFalse(stack.isEmpty());
         assertEquals(1, stack.size());
@@ -23,7 +27,6 @@ public class StackTest {
 
     @Test
     public void stackAndUnstackTwoElement() {
-        var stack = new MyStack<String>();
         stack.add("first");
         stack.add("second");
         assertEquals(2, stack.size());
