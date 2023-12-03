af16e6dbcfac872ec30fcb9c20835342e8136d6d
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-03 12:35:06 -0300

    Refactor the MyStack's methods to pass in the tests

--- src/tdd/ita/semana01/handson/firstSteps/MyStack.java
+++ src/tdd/ita/semana01/handson/firstSteps/MyStack.java
@@ -2,8 +2,10 @@ package tdd.ita.semana01.handson.firstSteps;
 
 public class MyStack {
     public boolean isEmpty() {
+        return true;
     }
 
     public int size() {
+        return 0;
     }
 }
