ca6567e94656ee121b3ea29c82e4ff2e7fb1cd6a
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-05 09:41:02 -0300

    Change the code behavior to pass in the second test (step green)

--- src/tdd/ita/semana02/handson/MyTranslator.java
+++ src/tdd/ita/semana02/handson/MyTranslator.java
@@ -1,15 +1,21 @@
 package tdd.ita.semana02.handson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyTranslator {
 
+    private final Map<String, String> wordsTranslate = new HashMap<>();
+
     public boolean isEmpty() {
-        return true;
+        return wordsTranslate.isEmpty();
     }
 
-    public void addWordTranslate(String bom, String good) {
+    public void addWordTranslate(String word, String translate) {
+        wordsTranslate.put(word, translate);
     }
 
-    public String translate(String bom) {
-        return null;
+    public String translate(String word) {
+        return wordsTranslate.get(word);
     }
 }
