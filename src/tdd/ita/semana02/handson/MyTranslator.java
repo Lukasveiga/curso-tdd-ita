52c0d7cbe6e5c01215a0f0db6610abbfa4f40f5b
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-05 09:32:10 -0300

    Change the behavior of the frist test

--- src/tdd/ita/semana02/handson/MyTranslator.java
+++ src/tdd/ita/semana02/handson/MyTranslator.java
@@ -1,7 +1,8 @@
 package tdd.ita.semana02.handson;
 
 public class MyTranslator {
-    public String translate(String s) {
-        return s;
+
+    public boolean isEmpty() {
+        return true;
     }
 }
--- src/tdd/ita/semana02/handson/TranslatorTest.java
+++ src/tdd/ita/semana02/handson/TranslatorTest.java
@@ -2,13 +2,13 @@ package tdd.ita.semana02.handson;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TranslatorTest {
 
     @Test
     public void translatorWithoutWords() {
         MyTranslator translator = new MyTranslator();
-        String result = translator.translate("");
-        assertEquals("", result);
+        assertTrue(translator.isEmpty());
     }
 }
