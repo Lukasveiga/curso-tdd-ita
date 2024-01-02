e4b7b2e46cd1836354c1298adabd2bc7d4345fd9
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-02 16:33:29 -0300

    feat: Add frist code example of Refactoring book by martin flower

---
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/handson/Rental.java
@@ -0,0 +1,17 @@
+package tdd.ita.semana04.handson;
+
+public class Rental {
+
+    private final Movie _movie;
+    private final int _daysRented;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+    public int getDaysRented() {
+        return _daysRented;
+    }
+    public Movie getMovie() {
+        return _movie;
+    }
+}
