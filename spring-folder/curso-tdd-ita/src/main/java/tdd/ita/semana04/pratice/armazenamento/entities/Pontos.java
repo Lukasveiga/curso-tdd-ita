bd73302e611e8f460077bab1c0f724a89ffd6373
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-05 14:49:20 -0300

    feat: Change Pontos record to a class and insert setPontos method

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Pontos.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Pontos.java
@@ -1,3 +1,26 @@
 package tdd.ita.semana04.pratice.armazenamento.entities;
 
-public record Pontos(String tipo, int pontuacao) {}
+public class Pontos {
+
+    private final String tipo;
+
+    private int pontos;
+
+
+    public Pontos(String tipo, int pontos) {
+        this.tipo = tipo;
+        this.pontos = pontos;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos += pontos;
+    }
+}
