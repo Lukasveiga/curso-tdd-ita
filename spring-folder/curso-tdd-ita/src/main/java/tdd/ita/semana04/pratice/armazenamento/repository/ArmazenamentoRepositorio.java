1643a10efe2428fb3ae213720f4b3bd6b706c31b
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-05 14:50:31 -0300

    refactor: Change ArmazenamentoRepositorio method signature

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorio.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorio.java
@@ -1,9 +1,8 @@
 package tdd.ita.semana04.pratice.armazenamento.repository;
 
-import tdd.ita.semana04.pratice.armazenamento.entities.Pontos;
 import tdd.ita.semana04.pratice.armazenamento.entities.Usuario;
 
 public interface ArmazenamentoRepositorio {
 
-    void salvarPontosParaUsuario(Usuario usuario, Pontos pontos);
+    void salvarUsuario(Usuario usuario);
 }
