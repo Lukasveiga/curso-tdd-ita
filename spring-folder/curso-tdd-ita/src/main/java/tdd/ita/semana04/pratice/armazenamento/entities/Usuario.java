90ecd57b5f49378cf5cb8164407ffeb68345c524
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-05 14:48:57 -0300

    feat: Change Usuario record to a class and insert param listaDePontos

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Usuario.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Usuario.java
@@ -1,3 +1,29 @@
 package tdd.ita.semana04.pratice.armazenamento.entities;
 
-public record Usuario(String nome) {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+
+    private final String nome;
+
+    private final List<Pontos> listaDePontos = new ArrayList<>();
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void addPontos(Pontos pontos) {
+        for (Pontos p : listaDePontos) {
+            if(p.getTipo().equals(pontos.getTipo())) {
+                p.setPontos(pontos.getPontos());
+                return;
+            }
+        }
+        listaDePontos.add(pontos);
+    }
+}
