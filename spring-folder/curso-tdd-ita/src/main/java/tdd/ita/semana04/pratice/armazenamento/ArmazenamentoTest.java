180d64245687e599a42d67663b71bc3262dc3e1c
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-08 15:52:07 -0300

    refactor: Inject a list of Usuarios entity + change Usuario information provided

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/ArmazenamentoTest.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/ArmazenamentoTest.java
@@ -7,43 +7,53 @@ import tdd.ita.semana04.pratice.armazenamento.entities.Pontos;
 import tdd.ita.semana04.pratice.armazenamento.entities.Usuario;
 import tdd.ita.semana04.pratice.armazenamento.repository.ArmazenamentoRepositorioInMemory;
 
+import java.util.Arrays;
+
 public class ArmazenamentoTest {
 
     private Armazenamento armazenamento;
 
     @BeforeEach
     public void setUp() {
-        ArmazenamentoRepositorioInMemory armazenamentoRepositorioInMemory = new ArmazenamentoRepositorioInMemory();
+        var listaUsuarios = Arrays.asList(
+                new Usuario("Guerra"), new Usuario("Ana"), new Usuario("Jorge")
+        );
+        ArmazenamentoRepositorioInMemory armazenamentoRepositorioInMemory = new ArmazenamentoRepositorioInMemory(listaUsuarios);
         armazenamento = new Armazenamento(armazenamentoRepositorioInMemory);
     }
 
     @Test
     public void armazenarPontuacaoParaUmUsuario() {
-        var usuario = new Usuario("Guerra");
+        var nomeUsuario = "Guerra";
         var pontuacao = new Pontos("estrela",10);
-        var resultado = armazenamento.armazenar(usuario, pontuacao);
+        var resultado = armazenamento.armazenar(nomeUsuario, pontuacao);
         Assertions.assertThat(resultado)
                 .isEqualTo(String.format("O usuário %s recebeu %d pontos do tipo %s",
-                        usuario.getNome(), pontuacao.getPontos(), pontuacao.getTipo()));
+                        nomeUsuario, pontuacao.pontos(), pontuacao.tipo()));
     }
 
     @Test
     public void recuperarPontosUsuario() {
-        var usuario = new Usuario("Guerra");
+        var nomeUsuario = "Guerra";
         var pontuacao = new Pontos("estrela",10);
-        armazenamento.armazenar(usuario, pontuacao);
-        var pontosUsuario = armazenamento.recuperarPontosDeUsuario(usuario, pontuacao.getTipo());
-        Assertions.assertThat(pontosUsuario).isEqualTo(pontuacao.getPontos());
+        armazenamento.armazenar(nomeUsuario, pontuacao);
+        var pontosUsuario = armazenamento.recuperarPontosDeUsuario(nomeUsuario, pontuacao.tipo());
+        Assertions.assertThat(pontosUsuario).isEqualTo(pontuacao.pontos());
     }
 
     @Test
     public void recuperarPontosUsuarioValoresDobrados() {
-        var usuario = new Usuario("Guerra");
+        var nomeUsuario = "Guerra";
         var pontuacao = new Pontos("estrela",10);
         var pontuacao2 = new Pontos("estrela",10);
-        armazenamento.armazenar(usuario, pontuacao);
-        armazenamento.armazenar(usuario, pontuacao2);
-        var pontosUsuario = armazenamento.recuperarPontosDeUsuario(usuario, pontuacao.getTipo());
-        Assertions.assertThat(pontosUsuario).isEqualTo(pontuacao.getPontos() + pontuacao2.getPontos());
+        armazenamento.armazenar(nomeUsuario, pontuacao);
+        armazenamento.armazenar(nomeUsuario, pontuacao2);
+        var pontosUsuario = armazenamento.recuperarPontosDeUsuario(nomeUsuario, pontuacao.tipo());
+        Assertions.assertThat(pontosUsuario).isEqualTo(pontuacao.pontos() + pontuacao2.pontos());
+    }
+
+    @Test
+    public void retornarUsuariosComPontuacao() {
+
     }
 }
