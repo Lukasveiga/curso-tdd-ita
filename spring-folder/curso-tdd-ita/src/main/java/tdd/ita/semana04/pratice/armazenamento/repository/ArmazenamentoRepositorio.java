71a090b6b01085a40a0bc8e310dfd98b5267a9d6
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-08 15:49:47 -0300

    feat/refactor: Change return of the salvarPontosParaUsuario method and created buscarUsuario method

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorio.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorio.java
@@ -1,8 +1,13 @@
 package tdd.ita.semana04.pratice.armazenamento.repository;
 
+import tdd.ita.semana04.pratice.armazenamento.entities.Pontos;
 import tdd.ita.semana04.pratice.armazenamento.entities.Usuario;
+
+import java.util.Optional;
 
 public interface ArmazenamentoRepositorio {
 
-    void salvarUsuario(Usuario usuario);
+    void salvarPontosParaUsuario(Usuario usuario, Pontos pontos);
+
+    Optional<Usuario> buscarUsuario(String nomeUsuario);
 }
