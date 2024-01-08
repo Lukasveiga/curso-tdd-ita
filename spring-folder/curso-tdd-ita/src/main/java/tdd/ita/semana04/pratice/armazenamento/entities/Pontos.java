8771ce99f8cc0074e2597728a2896d2227f9c936
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-08 15:50:06 -0300

    refactor: Change Pontos class to a record

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Pontos.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/entities/Pontos.java
@@ -1,22 +1,3 @@
 package tdd.ita.semana04.pratice.armazenamento.entities;
 
-public class Pontos {
-
-    private final String tipo;
-
-    private final int pontos;
-
-
-    public Pontos(String tipo, int pontos) {
-        this.tipo = tipo;
-        this.pontos = pontos;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public int getPontos() {
-        return pontos;
-    }
-}
+public record Pontos(String tipo, int pontos) {}
