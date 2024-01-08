9eee0c69f51123a84b5b1b40744c35e8afbb947d
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-08 15:52:52 -0300

    feat: Add salvarPontosParaUsuario and buscarUsuario method to ArmazenamentoRepositorioInMemory in memory repository

--- spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorioInMemory.java
+++ spring-folder/curso-tdd-ita/src/main/java/tdd/ita/semana04/pratice/armazenamento/repository/ArmazenamentoRepositorioInMemory.java
@@ -1,16 +1,26 @@
 package tdd.ita.semana04.pratice.armazenamento.repository;
 
+import tdd.ita.semana04.pratice.armazenamento.entities.Pontos;
 import tdd.ita.semana04.pratice.armazenamento.entities.Usuario;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ArmazenamentoRepositorioInMemory implements ArmazenamentoRepositorio {
 
-    private final List<Usuario> usuarios = new ArrayList<>();
+    private final List<Usuario> usuarios;
+
+    public ArmazenamentoRepositorioInMemory(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 
     @Override
-    public void salvarUsuario(Usuario usuario) {
+    public void salvarPontosParaUsuario(Usuario usuario, Pontos pontos) {
+        usuario.addPontos(pontos);
+    }
 
+    public Optional<Usuario> buscarUsuario(String nomeUsuario) {
+        return this.usuarios.stream()
+                .filter(u -> u.getNome().equalsIgnoreCase(nomeUsuario)).findFirst();
     }
 }
